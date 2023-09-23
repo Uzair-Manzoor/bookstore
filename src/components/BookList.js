77497f804bd55f90f4c258a436043ffd7bd94d47
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-09-24 01:56:26 +0500

    update BookList component to render each book while mapping

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Book from './book'; // Import the Book component
 
 const BookList = ({ books }) => (
   <ul className="books">
     {books.map((book) => (
-      <books key={book.id} title={book.title} />
+      <Book key={book.id} title={book.title} author={book.author} />
     ))}
   </ul>
 );
@@ -14,6 +15,7 @@ BookList.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
     }),
   ).isRequired,
 };
